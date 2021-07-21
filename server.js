5854613a5443721a410ef4146bdd24ffe65ab143
Mugabo Uwilingiyimana <thierry.uwi@gmail.com>
2021-07-21 17:00:20 -0400

    our initial commit - end of july 21

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
+
 
 const port = process.env.PORT || 8000;
 const app = express();
 const staticDir = process.env.DEV ? "./client/public" : "./client/build";
 
 app.use(express.static(staticDir));
+
+app.use(express.urlencoded({extended: true});
+
+// setup mongoose
+const atlasDbUrl = "mongodb://localhost:27017"
+mongoose.connect(altasDbUrl,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+
+
+// set error message
+db.on('error', console.error.bind(console, 'conection error'))
+
+
+
+
 
 
 
